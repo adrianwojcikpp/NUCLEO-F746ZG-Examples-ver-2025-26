a46a16b21d17ebf6ef9f2370ef28092a7d8148c2
Adrian Wójcik <54798218+adrianwojcikpp@users.noreply.github.com>
2025-10-12 18:00:39 +0200

    L04: TIM - Task #4

--- L03_Example/Core/Inc/main.h
+++ L03_Example/Core/Inc/main.h
@@ -59,6 +59,9 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define USER_Btn_Pin GPIO_PIN_13
 #define USER_Btn_GPIO_Port GPIOC
+#define USER_Btn_EXTI_IRQn EXTI15_10_IRQn
+#define DEBUG_OUT2_Pin GPIO_PIN_3
+#define DEBUG_OUT2_GPIO_Port GPIOF
 #define MCO_Pin GPIO_PIN_0
 #define MCO_GPIO_Port GPIOH
 #define RMII_MDC_Pin GPIO_PIN_1
